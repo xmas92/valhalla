0bd9423e1508830cd1a1f38f7f8d253d24f9b017
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2026-05-06 19:13:27 +0000

    JDK-8334334 never ran the added test method

--- test/langtools/tools/javac/processing/model/TestSourceVersion.java
+++ test/langtools/tools/javac/processing/model/TestSourceVersion.java
@@ -44,6 +44,7 @@ public class TestSourceVersion {
         testRestrictedKeywords();
         testVar();
         testYield();
+        testValue();
         testValueOfRV();
         testRuntimeVersion();
     }
